b6d542ceaa9a276fe82c9bbf5f88754170476b2c
miecz <miecz@LAPTOP-UVM3H6HN>
2022-12-02 12:13:29 +0100

    extension methods

--- DateTimeApplication/Program.cs
+++ DateTimeApplication/Program.cs
@@ -10,7 +10,17 @@ namespace DateTimeApplication
             //DateTimeModification();
             //DateTimeFormatting();
             //TimeMeasurement();
-            DateTimeHelpers();
+            //DateTimeHelpers();
+
+            DateTime now = DateTime.Now;
+
+            DateTime before = now.Subtract(new TimeSpan(7, 0, 0, 0));
+
+            DateTime after = now.AddDays(7);
+
+            bool isDateBetween = Utils.IsDateBetween(now, before, after);
+
+            bool isDateBetween2 = now.IsBetween(before, after);
         }
 
         static void DateTimeModification()
