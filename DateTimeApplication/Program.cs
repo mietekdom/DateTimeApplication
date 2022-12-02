07746f0909d8950a3d72b559b7e08b6f6e3a7ee4
miecz <miecz@LAPTOP-UVM3H6HN>
2022-12-02 11:03:52 +0100

    Add project files.

---
+++ DateTimeApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DateTimeApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTimeModification();
+        }
+
+        static void DateTimeModification()
+        {
+            DateTime now = DateTime.Now;
+            DateTime openDate = new DateTime(1982, 6, 17);
+        }
+    }
+}
