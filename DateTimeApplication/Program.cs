2828c270132e2dc0692a3cb686e711e8aa655451
miecz <miecz@LAPTOP-UVM3H6HN>
2022-12-02 11:26:25 +0100

    working on DateTime

--- DateTimeApplication/Program.cs
+++ DateTimeApplication/Program.cs
@@ -6,13 +6,32 @@ namespace DateTimeApplication
     {
         static void Main(string[] args)
         {
-            DateTimeModification();
+            //DateTimeModification();
+            DateTimeFormatting();
         }
 
         static void DateTimeModification()
         {
             DateTime now = DateTime.Now;
-            DateTime openDate = new DateTime(1982, 6, 17);
+            DateTime openDate = new DateTime(1982, 2, 21);
+
+            TimeSpan result = now - openDate;
+
+            Console.WriteLine(result.Days);
+            Console.WriteLine(result.TotalSeconds);
+
+            DateTime expireDate = now.AddDays(22);
+
+            Console.WriteLine(expireDate);
+        }
+        static void DateTimeFormatting()
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToString("g"));
+            Console.WriteLine(now.ToString("G"));
+            Console.WriteLine(now.ToString("yyyy-MM-dd")); //
         }
     }
 }
